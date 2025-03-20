aa087e17ead93c8a2fe59c195cefdd5b4970ada5
Joshei <joshuaeirm@gmail.com>
2025-03-20 14:04:48 -0400

    Before commit

--- src/routes/category/category-component.jsx
+++ src/routes/category/category-component.jsx
@@ -1,5 +1,5 @@
 
-//
+// this is for rebase - a commit
 // worked on max width for site: 4000k, works besides checkboxes, and centering.  Broke other things while doing this, shouldn't take very long to fix.  look at width and height functionality and decide if it is still needed.
 //
 import { React, useState, useEffect, useMemo, useRef} from "react";

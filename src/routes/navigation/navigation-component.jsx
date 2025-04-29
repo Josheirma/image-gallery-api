d58053dee3f8afc067a658b3f3e5657b37d0079b
Josheirma <joshuaeirman@gmail.com>
2025-04-29 17:53:10 -0400

    commit

--- src/routes/navigation/navigation-component.jsx
+++ src/routes/navigation/navigation-component.jsx
@@ -1,13 +1,12 @@
 import { Outlet} from "react-router-dom";
-import { NavigationContainer} from "./navigation-styles";
+
 
 
 const Navigation = () => {
  return (
    //Outlet is where child routes in router code (App.js) of <navigation/> go
-    <NavigationContainer>
-       <Outlet />
-    </NavigationContainer>
+   <Outlet />
+    
   );
 };
 export default Navigation;
